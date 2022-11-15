fdb93a3d14ea1712e7f2c2178d84e3b3170f50d9
NizamSadaf <nizamsadaf1460@gmail.com>
2022-11-16 00:14:54 +0600

    Carousel heading position changed

--- src/Components/MyCarousel.jsx
+++ src/Components/MyCarousel.jsx
@@ -9,29 +9,32 @@ import img3 from '../assets/3.jpg'
 import img4 from '../assets/4.jpg'
 const MyCarousel = () => {
   const headingOption={
-    display:'flex',
-    flexDirection:'column',
-    alignItems:'center',
-    justifyContent:'center',
-    textTransform:'uppercase'
+    // display:'flex',
+    // flexDirection:'column',
+    // alignItems:'center',
+    // justifyContent:'center',
+    // textTransform:'uppercase'
+    position:'absolute',
+    left:'50%',
+    transform:'translate(-50%,-50%)'
   }
   return (
     <Carousel interval={1000} autoFocus={false} autoPlay={true} showArrows={false} showIndicators showThumbs={false} stopOnHover={false}>
       <Box  w={'full'} h={'100vh'}>
-      <Heading bg={'whiteAlpha.400'} w={'fit-content'} m={'auto'} {...headingOption} color={'black'}>Welcome to Gaming World</Heading>
-        <Image src={img1}/>
+      <Heading bg={'whiteAlpha.400'} w={'fit-content'} m={'auto'} top={['30%','50%']} {...headingOption} color={'black'}>Welcome to Gaming World</Heading>
+        <Image src={img1}  h={['70vh','']}/>
       </Box>
       <Box w={'full'} h={'100vh'}>
-      <Heading bg={'whiteAlpha.800'} {...headingOption} color={'black'}>Welcome to Gaming World</Heading>
-        <Image src={img2}/>
+      <Heading top={['30%','50%']} bg={'whiteAlpha.600'} {...headingOption} color={'black'}>Welcome to Gaming World</Heading>
+        <Image src={img2} h={['70vh','']}/>
       </Box> 
       <Box w={'full'} h={'100vh'}>
-      <Heading bg={'blackAlpha.900'} {...headingOption} color={'white'}>Welcome to Gaming World</Heading>
-        <Image src={img3}/>
+      <Heading top={['30%','50%']} bg={'whiteAlpha.500'} {...headingOption} color={'white'}>Welcome to Gaming World</Heading>
+        <Image src={img3} h={['70vh','']}/>
       </Box>
       <Box w={'full'} h={'100vh'}>
-      <Heading bg={'blackAlpha.900'} {...headingOption} color={'white'}>Welcome to Gaming World</Heading>
-        <Image src={img4}/>
+      <Heading top={['30%','50%']} bg={'blackAlpha.900'} {...headingOption} color={'white'}>Welcome to Gaming World</Heading>
+        <Image src={img4} h={['70vh','']}/>
       </Box>
     </Carousel>
   )
